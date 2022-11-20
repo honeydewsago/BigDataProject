4851d0c2577a13e0517cc5e6b84041577e227058
Jia Wei <jiawei392@gmail.com>
2022-11-20 17:34:58 +0800

    Update descriptive analysis and add visualisations for both dataset

--- data_analysis.R
+++ data_analysis.R
@@ -1,55 +1,115 @@
 install.packages("GGally")
+install.packages("pastecs")
 
 library(ggplot2)
 library(dplyr)
+library(ggpubr)
 library(GGally)
 library(car)
+library(pastecs)
+
 
 #objective 1
 #gender transaction data
-gender_transactions <-read.csv("C:/Sem6/5011CEM BigData/BigDataProject/gender_transactions.csv")
-View(gender_transactions)
-summary(gender_transactions)
+gender_trans <-read.csv("C:/Sem6/5011CEM BigData/BigDataProject/gender_transactions.csv")
+View(gender_trans)
+
+#descriptive analysis
+#display first 6 observations
+head(gender_trans)
+
+#display the structure of the dataset
+str(gender_trans)
+
+#compute the min, max, mean, median, 1st quartile and 3rd quartile for each attribute
+summary(gender_trans)
+
+#convert scientific notation numbers to non-scientific notation
+options(scipen=100)
+options(digits=3)
+
+#additionally compute sum, range, standard deviation, variance and coefficients
+#include skewness, kurtosis and normality test
+stat.desc(gender_trans, norm = TRUE)
+
+#histogram
+#number of transactions
+hist(gender_trans$num_transactions,
+     main="Histogram of Number of Transactions",
+     xlab="Number of Transactions",
+     col="wheat")
+abline(v = mean(gender_trans$num_transactions), col = 'red', lty = 2)
+
+#male
+hist(gender_trans$male,
+     main="Histogram of Male Population",
+     xlab="Male Population",
+     col="wheat")
+abline(v = mean(gender_trans$male), col = 'red', lty = 2)
+
+#female
+hist(gender_trans$female,
+     main="Histogram of Female Population",
+     xlab="Female Population",
+     col="wheat")
+abline(v = mean(gender_trans$female), col = 'red', lty = 2)
+
+#population
+hist(gender_trans$population,
+     main="Histogram of Overall Population",
+     xlab="Overall Population",
+     col="wheat")
+abline(v = mean(gender_trans$population), col = 'red', lty = 2)
+
+#box plot
+#boxplot(gender_trans$num_transactions)
+#df <- data.frame()
 
 #correlation test
 mosthighlycorrelated <- function(mydataframe,numtoreport)
 {
+  # find the correlations
   cormatrix <- cor(mydataframe)
+  # set the correlations on the diagonal or lower triangle to zero,
+  # so they will not be reported as the highest ones:
   diag(cormatrix) <- 0
   cormatrix[lower.tri(cormatrix)] <- 0
+  # flatten the matrix into a dataframe for easy sorting
   fm <- as.data.frame(as.table(cormatrix))
+  # assign human-friendly names
   names(fm) <- c("First.Variable", "Second.Variable","Correlation")
+  # sort and print the top n correlations
   head(fm[order(abs(fm$Correlation),decreasing=T),],n=numtoreport)
 }
 
-model <- select(gender_transactions, 'num_transactions', 'male', 'female')
+model <- select(gender_trans, 'num_transactions', 'male', 'female')
 mosthighlycorrelated(model, 20)
 
 #calculate correlation value
-cor.test(formula = ~ num_transactions + male, data = gender_transactions,
+cor.test(formula = ~ num_transactions + male, data = gender_trans,
          method = "pearson", use = "complete.obs")
-cor.test(formula = ~ num_transactions + female, data = gender_transactions,
+cor.test(formula = ~ num_transactions + female, data = gender_trans,
          method = "pearson", use = "complete.obs")
 
 #create scatterplot matrix
-ggpairs(gender_transactions, columns = c( "male","female", "num_transactions"), 
+ggpairs(gender_trans, columns = c( "male","female", "num_transactions"), 
         title = "Scatterplot of transactions number and populataion gender(Borough Area)", 
         upper = list(continuous = wrap("cor",size = 3)),
         lower = list(continuous = wrap("smooth",alpha = 1,size = 1,color = "blue")))
 
 #create scatterplot with fitted regression line
-ggplot(gender_transactions, aes(x = male, y = num_transactions)) +
+ggplot(gender_trans, aes(x = male, y = num_transactions)) +
   geom_point() +
   stat_smooth(method = "lm")
-ggplot(gender_transactions, aes(x = female, y = num_transactions)) +
+ggplot(gender_trans, aes(x = female, y = num_transactions)) +
   geom_point() +
   stat_smooth(method = "lm")
 
 #hypothesis test
 #calculate p-value
-model<- lm(num_transactions ~ male, data = gender_transactions)
+model<- lm(num_transactions ~ male, data = gender_trans)
 summary(model)
-model<- lm(num_transactions ~ female, data = gender_transactions)
+model<- lm(num_transactions ~ female, data = gender_trans)
 summary(model)
 
 #plot ?
@@ -59,56 +119,104 @@ plot(model)
 
 #objective 2
 #age group transaction data
-age_transactions <-read.csv("C:/Sem6/5011CEM BigData/BigDataProject/age_group_transactions.csv")
-View(age_transactions)
-summary(age_transactions)
+age_trans <-read.csv("C:/Sem6/5011CEM BigData/BigDataProject/age_group_transactions.csv")
+View(age_trans)
+
+#descriptive analysis
+#display first 6 observations
+head(age_trans)
+
+#display the structure of the dataset
+str(age_trans)
+
+#compute the min, max, mean, median, 1st quartile and 3rd quartile for each attribute
+summary(age_trans)
+
+#additionally compute sum, range, standard deviation, variance and coefficients
+#include skewness, kurtosis and normality test
+stat.desc(age_trans, norm = TRUE)
+
+#histogram
+#number of transactions
+hist(age_trans$num_transactions,
+     main="Histogram of Number of Transactions",
+     xlab="Number of Transactions",
+     col="wheat")
+abline(v = mean(age_trans$num_transactions), col = 'red', lty = 2)
+
+#average age
+hist(age_trans$avg_age,
+     main="Histogram of Average Population Age",
+     xlab="Average Population Age",
+     col="wheat")
+abline(v = mean(age_trans$avg_age), col = 'red', lty = 2)
+
+#age_0_17
+hist(age_trans$age_0_17,
+     main="Histogram of Age Group 0 to 17",
+     xlab="Age Group 0 to 17",
+     col="wheat")
+abline(v = mean(age_trans$age_0_17), col = 'red', lty = 2)
+
+#age_18_64
+hist(age_trans$age_18_64,
+     main="Histogram of Age Group 18 to 64",
+     xlab="Age Group 18 to 64",
+     col="wheat")
+abline(v = mean(age_trans$age_18_64), col = 'red', lty = 2)
+
+#age_65+
+hist(age_trans$age_65.,
+     main="Histogram of Age Group 65 and Above",
+     xlab="Age Group 65 and Above",
+     col="wheat")
+abline(v = mean(age_trans$age_65.), col = 'red', lty = 2)
 
 #correlation test
-model <- select(age_transactions, 'num_transactions', 'age_0_17', 'age_18_64', 'age_65.', 'avg_age')
+model <- select(age_trans, 'num_transactions', 'age_0_17', 'age_18_64', 'age_65.', 'avg_age')
 mosthighlycorrelated(model, 20)
 
 #calculate correlation value
-cor.test(formula = ~ num_transactions + age_0_17, data = age_transactions,
+cor.test(formula = ~ num_transactions + age_0_17, data = age_trans,
          method = "pearson", use = "complete.obs")
-cor.test(formula = ~ num_transactions + age_18_64, data = age_transactions,
+cor.test(formula = ~ num_transactions + age_18_64, data = age_trans,
          method = "pearson", use = "complete.obs")
-cor.test(formula = ~ num_transactions + age_65., data = age_transactions,
+cor.test(formula = ~ num_transactions + age_65., data = age_trans,
          method = "pearson", use = "complete.obs")
-cor.test(formula = ~ num_transactions + avg_age, data = age_transactions,
+cor.test(formula = ~ num_transactions + avg_age, data = age_trans,
          method = "pearson", use = "complete.obs")
 
 #create scatterplot matrix
-ggpairs(age_transactions, columns = c( "age_0_17","age_18_64", "age_65.", "avg_age","num_transactions"), 
+ggpairs(age_trans, columns = c( "age_0_17","age_18_64", "age_65.", "avg_age","num_transactions"), 
         title = "Scatterplot of transactions number and population age(Borough Area)", 
         upper = list(continuous = wrap("cor",size = 3)),
         lower = list(continuous = wrap("smooth",alpha = 1,size = 1,color = "blue")))
 
 #create scatterplot with fitted regression line
-ggplot(age_transactions, aes(x = age_0_17, y = num_transactions)) +
+ggplot(age_trans, aes(x = age_0_17, y = num_transactions)) +
   geom_point() +
   stat_smooth(method = "lm")
-ggplot(age_transactions, aes(x = age_18_64, y = num_transactions)) +
+ggplot(age_trans, aes(x = age_18_64, y = num_transactions)) +
   geom_point() +
   stat_smooth(method = "lm")
-ggplot(age_transactions, aes(x = age_65., y = num_transactions)) +
+ggplot(age_trans, aes(x = age_65., y = num_transactions)) +
   geom_point() +
   stat_smooth(method = "lm")
-ggplot(age_transactions, aes(x = avg_age, y = num_transactions)) +
+ggplot(age_trans, aes(x = avg_age, y = num_transactions)) +
   geom_point() +
   stat_smooth(method = "lm")
 
 
 #hypothesis test
 #calculate p-value
-model<- lm(num_transactions ~ age_0_17, data = age_transactions)
+model<- lm(num_transactions ~ age_0_17, data = age_trans)
 summary(model)
-model<- lm(num_transactions ~ age_18_64, data = age_transactions)
+model<- lm(num_transactions ~ age_18_64, data = age_trans)
 summary(model)
-model<- lm(num_transactions ~ age_65., data = age_transactions)
+model<- lm(num_transactions ~ age_65., data = age_trans)
 summary(model)
-model<- lm(num_transactions ~ avg_age, data = age_transactions)
+model<- lm(num_transactions ~ avg_age, data = age_trans)
 summary(model)
 
 #plot ?
 plot(model)
-
